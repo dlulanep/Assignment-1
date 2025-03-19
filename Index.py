3858b6258d5c9f090d67a4f103be8f2a85f34a72
dlulanep <74797289+dlulanep@users.noreply.github.com>
2025-03-19 03:11:11 +0200

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,20 @@
+operator = input("Enter an operator (+,-,*,/)")
+num1 = float(input("Enter the 1st number:"))
+num2 = float(input("Enter the 2nd number:"))
+
+if operator == "+":
+    print(num1+num2)
+    print("The sum of the numbers is",num1+num2)
+elif operator == "-":
+    print(num1-num2)
+    print("The difference of the numbers is".num1-num2))
+elif operator == "*":
+     print(num1*num2)
+     print("The product of the numbers is",num1*num2))
+elif operator == "/":
+    print(num1/num2)
+    print("The division of the numbers is",num1/num2))
+  else
+    print("Invalid operator")
+         
+       
